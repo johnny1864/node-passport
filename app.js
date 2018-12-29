662b636b35370ea3ed81bc2670679a685f8f387f
johnny1864 <johnny1864@yahoo.com>
2018-12-29 13:52:21 -0800

    Connected app to database and created user model

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+
+// DB CONNENTION
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
 
 //EJS
 app.use(expressLayouts);
@@ -11,7 +23,7 @@ const indexRoute = require("./routes/index");
 const userRoute = require("./routes/user");
 
 app.use("/", indexRoute);
-app.use("/user", userRoute);
+app.use("/users", userRoute);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://johnny123:Johnny123@cluster0-tohv5.mongodb.net/test?retryWrites=true"
+};
