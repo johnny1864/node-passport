018615190de6474fe639812159375477dd2d4a5f
johnny1864 <johnny1864@yahoo.com>
2018-12-29 13:05:58 -0800

    added view folder with ejs file and updated routes

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+//EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+//ROUTES
+const indexRoute = require("./routes/index");
+const userRoute = require("./routes/user");
+
+app.use("/", indexRoute);
+app.use("/user", userRoute);
 
 const PORT = process.env.PORT || 5000;
 
